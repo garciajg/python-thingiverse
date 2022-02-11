92722ea2ae59b72e7bd6260205a725ea956072ef
Jose Garcia <jose.garcia@vokal.io>
2022-02-10 23:24:58 -0700

    documentation added

--- thingiverse/types/exceptions.py
+++ thingiverse/types/exceptions.py
@@ -1,15 +1,19 @@
 
 class SearchException(Exception):
+    """Used when something went wrong when searching"""
     pass
 
 
 class ThingiverseException(Exception):
+    """Used when a general error happens"""
     pass
 
 
 class UnathenticatedException(Exception):
+    """Used when user is unathenticated"""
     pass
 
 
 class ResourceNotFound(Exception):
+    """Used when a resource like a Thing isn't found"""
     pass
